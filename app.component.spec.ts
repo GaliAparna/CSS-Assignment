09123320b658ffd666d6e43a689521d1dbde0505
GaliAparna <60088550+GaliAparna@users.noreply.github.com>
2020-01-24 04:06:18 -0800

    Add files via upload

--- app.component.spec.ts
+++ app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proj04'`, () => {
+  it(`should have as title 'proj05'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('proj04');
+    expect(app.title).toEqual('proj05');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('proj04 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('proj05 app is running!');
   });
 });
--- app.component.ts
+++ app.component.ts
@@ -6,30 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private FirstNumber:number=null;
-  private SecondNumber:number=null;
-  private result:number=null;
+ private message:string=null;
+ private version:number=null;
+ private topics:string[]=null;
+ private inx :number=null;
+ public constructor(){
+   this.message="welcome";
+   this.version=8;
+   this.topics=['Data Binding','Forms','Http','Routing'];
+ 
+ }
+ public addTopic(currentTopic:string):boolean{
+   if(currentTopic==""){
+     alert("Add topic");
+     return false;
+   }
+   for(var topic of this.topics){
+     if(currentTopic==topic)
+     alert("Duplicate Value");
+   }
+   this.topics.push(currentTopic);
+ }
+ public delTopic(idx:number):void{
+   this.topics.splice(idx,1);
+ }
+ public searchTopic(currentTopic:String):void{
+ 
+     if(currentTopic==" "){
+        alert("No Topics to display")
+     }
+   for(var topic of this.topics){
+     
+
+    if(currentTopic==topic){
+      
+    alert(currentTopic);
+   }
   
- 
-public getAdd(): void{
-  this.result=this.FirstNumber+ this.SecondNumber;
-  
-}
-public getSub():void{
-  this.result=this.FirstNumber-this.SecondNumber;
- 
-}
-public getMul(): void{
-  this.result=this.FirstNumber*this.SecondNumber;
- 
-}
-public getDiv(): void{
-  this.result=this.FirstNumber/this.SecondNumber;
+  }
 
-}
+ }
 
-
+  public deleteTopic(currentTopic:string):void{
+    if(currentTopic==""){
+    alert("no topics to delete");
+    }
+    for(var topic of this.topics){
+      if(currentTopic==topic){
+        this.inx=this.topics.indexOf(currentTopic);
+        this.topics.splice(this.inx,1);
+      }
+    }
   
     
-  
+    
+  }
+
+
+   
+ 
 }
